c9854d9715164e9fddc1f4913c43b8ad7543c988
Rohan Prakash Krishna Prakash <rohanprakash.7001@gmail.com>
2025-07-10 20:59:50 -0400

    Procfile

--- app.py
+++ app.py
@@ -13,24 +13,28 @@ scalar=pickle.load(open('scaling.pkl','rb'))
 def home():
     return render_template('home.html')
 
-@app.route('/predict_api',methods=['POST'])
+@app.route('/predict_api', methods=['POST'])
 def predict_api():
-    data=request.json['data']
-    print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
-    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
-    output=regmodel.predict(new_data)
-    print(output[0])
-    return jsonify(output[0])
+    input_json = request.get_json()
+    data = input_json.get('data', input_json)
 
-@app.route('/predict',methods=['POST'])
+    input_array = np.array(list(data.values())).reshape(1, -1)
+    new_data = scalar.transform(input_array)
+    output = regmodel.predict(new_data)[0]
+    output = max(0, output)
+
+    return f"The predicted house price is: ${output:.2f}"
+
+
+
+@app.route('/predict', methods=['POST'])
 def predict():
-    data=[float(x) for x in request.form.values()]
-    final_input=scalar.transform(np.array(data).reshape(1,-1))
-    print(final_input)
-    output=regmodel.predict(final_input)[0]
-    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
-
+    data = [float(x) for x in request.form.values()]
+    final_input = scalar.transform([data])  # scale the input
+    output = regmodel.predict(final_input)[0]
+    # Clamp to avoid negative price
+    output = max(0, output)
+    return render_template("home.html", prediction_text=f"The House price prediction is ${output:.2f}")
 
 
 if __name__=="__main__":
